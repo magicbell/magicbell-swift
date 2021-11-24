be7593727548b42451a1c839462e523d9ba4fe3a
Joan Martin <joan@mobilejazz.com>
2021-11-24 14:41:33 +0100

    Documenting interactor GetUserQuery

--- Source/Core/Features/User/Interactors/GetUserQueryInteractor.swift
+++ Source/Core/Features/User/Interactors/GetUserQueryInteractor.swift
@@ -14,11 +14,14 @@ struct GetUserQueryInteractor {
         self.getUserQuery = getUserQuery
     }
 
+    /// This methods returns the user query with the user credentials or throws an error if user is not authenticated yet.
     func execute() throws -> UserQuery {
         return try getUserQuery
             .execute(IdQuery("userQuery"), in: DirectExecutor())
             .mapError { error in
                 if error is CoreError.NotFound {
+                    // In this case, we want to throw an exception, as the user is attempting to perform an action without
+                    // having identified previously a user by email or userId.
                     return MagicBellError("Can't perform action because user is not identified. Please, call MagicBell.login to identify a user.")
                 }
                 return error

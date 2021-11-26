049ba59083beb278afae8138f23f958249fe1b93
Joan Martin <joan@mobilejazz.com>
2021-11-26 17:17:31 +0100

    fixing linter errors

--- Source/Core/Features/Store/StoreContext.swift
+++ Source/Core/Features/Store/StoreContext.swift
@@ -11,10 +11,10 @@ public struct StoreContext {
     public let name: String
     public let store: StorePredicate
     public let cursor: CursorPredicate
-
+    
     public init(_ name: String,
-         _ store: StorePredicate,
-         _ cursor: CursorPredicate) {
+                _ store: StorePredicate,
+                _ cursor: CursorPredicate) {
         self.name = name
         self.store = store
         self.cursor = cursor
